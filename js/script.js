f4c4fddcd8fafa6b30a25281b5adb6ca49765472
RamzesLys <ramzes311982@gmail.com>
2023-04-13 12:59:32 +0300

    header scroll

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 const wrap = document.querySelector('.wrapper');
 const desc = document.querySelector('.desc');
 const header = document.querySelector('.header');
+const description = document.querySelector('.desc_description');
 
-wrap.onmousewheel = () => {
-  header.style.backgroundColor = 'rgba(0, 0, 0, .2)'
-  console.log('scroll');
-}
-// if (wrap.clientWidth <= 1120) {
-//   desc.style.display = "none";
+// wrap.onmousewheel = () => {
+//   header.style.backgroundColor = 'rgba(0, 0, 0, .3)';
+//   header.style.padding = '0.5em 1em'
+//   console.log('scroll');
 // }
+
+wrap.addEventListener("mousewheel", () => {
+  console.log('scrolling');
+  header.style.backgroundColor = 'rgba(0, 0, 0, .3)';
+  header.style.padding = '0.5em 1em';
+
+})
+
+
 
