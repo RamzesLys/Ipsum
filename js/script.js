5c947e916a70f44f7fcf69db5522cc68303b63e2
RamzesLys <ramzes311982@gmail.com>
2023-04-13 11:32:33 +0300

    correct

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, Ipsum');
+const wrap = document.querySelector('.wrapper');
+const desc = document.querySelector('.desc');
+const header = document.querySelector('.header');
+
+wrap.onmousewheel = () => {
+  header.style.backgroundColor = 'rgba(0, 0, 0, .2)'
+  console.log('scroll');
+}
+// if (wrap.clientWidth <= 1120) {
+//   desc.style.display = "none";
+// }
+
