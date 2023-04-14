c0f07253e2a5c3c91c3c2befd8a17b576277b120
RamzesLys <ramzes311982@gmail.com>
2023-04-14 12:10:41 +0300

    header_scroll +

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
 const wrap = document.querySelector('.wrapper');
 const desc = document.querySelector('.desc');
-const header = document.querySelector('.header');
 const description = document.querySelector('.desc_description');
+
+
+const header = document.querySelector('.header');
+window.onmousewheel = () => {
+  if (window.scrollY >= 54) {
+    console.log('down');
+    header.classList.add('_scroll');
+  } else {
+    console.log('else');
+    header.classList.remove('_scroll');
+  }
+}
+
+// window.addEventListener('scroll', function() {
+//   if (window.scrollY >= 54) {
+//     header.classList.addClass('header--scrolling'); // Добавить класс, который будет менять стили
+//   } else {
+//     header.classList.removeClass('header--scrolling'); // Убрать
+//   }
+// });
+// function watchHeader(entries) {
+//   console.log();
+//   if (entries[0].isIntersecting) {
+//     entries[0].target.classList.remove('._scroll');
+//   } else {
+//     entries[0].target.classList.add('._scroll');
+//   }
+// }
+
+// const headerObserver = new IntersectionObserver(watchHeader,);
+// headerObserver.observe(headerElement);
+
+
 
 // wrap.onmousewheel = () => {
 //   header.style.backgroundColor = 'rgba(0, 0, 0, .3)';
@@ -9,12 +41,14 @@ const description = document.querySelector('.desc_description');
 //   console.log('scroll');
 // }
 
-wrap.addEventListener("mousewheel", () => {
-  console.log('scrolling');
-  header.style.backgroundColor = 'rgba(0, 0, 0, .3)';
-  header.style.padding = '0.5em 1em';
 
-})
+
+// wrap.addEventListener("mousewheel", () => {
+//   console.log('scrolling');
+//   header.style.backgroundColor = 'rgba(0, 0, 0, .3)';
+//   header.style.padding = '0.5em 1em';
+
+// })
 
 
 
