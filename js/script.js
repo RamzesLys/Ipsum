5cd1ac6239822212e3c9a64219407d4f74bc86de
RamzesLys <ramzes311982@gmail.com>
2023-04-18 18:20:36 +0300

    service-menu +

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ const wrap = document.querySelector('.wrapper');
 const desc = document.querySelector('.desc');
 const description = document.querySelector('.desc_description');
 const header = document.querySelector('.header');
-const main = document.querySelector('.main');
-const services = document.querySelector('.services');
-const footer = document.querySelector('.footer');
+// const main = document.querySelector('.main');
+// const services = document.querySelector('.services');
+// const footer = document.querySelector('.footer');
+const darkBg = document.querySelector('._eclipse');
 
 
 //СКРОЛ МИШІ ВНИЗ
@@ -60,21 +61,21 @@ const buttonServiceClose = document.querySelector('.services_block-hidden-close'
 
 buttonService.onclick = () => {
   servicesMenuHidden.classList.add('active_services');
-  header.classList.add('_eclipse');
-  main.classList.add('_eclipse');
-  desc.classList.add('_eclipse');
-  services.classList.add('_eclipse');
-  footer.classList.add('_eclipse');
+  darkBg.classList.add('_open')
+
+  // header.classList.add('_eclipse');
+  // main.classList.add('_eclipse');
+  // desc.classList.add('_eclipse');
+  // services.classList.add('_eclipse');
+  // footer.classList.add('_eclipse');
 }
 
 buttonServiceClose.onclick = () => {
   servicesMenuHidden.classList.remove('active_services');
-  header.classList.remove('_eclipse');
-  main.classList.remove('_eclipse');
-  desc.classList.remove('_eclipse');
-  services.classList.remove('_eclipse');
-  footer.classList.remove('_eclipse');
-
-
-
+  darkBg.classList.remove('_open')
+  // header.classList.remove('_eclipse');
+  // main.classList.remove('_eclipse');
+  // desc.classList.remove('_eclipse');
+  // services.classList.remove('_eclipse');
+  // footer.classList.remove('_eclipse');
 }
